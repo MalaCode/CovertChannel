1f2e29380322ca20ec3d9092282a6450c82ead99
Malacode <jonathanchee15@gmail.com>
2018-04-28 21:30:22 -0700

    slightly cleaner update

--- channel.c
+++ channel.c
@@ -10,10 +10,11 @@
 #include <pthread.h>
 #include <errno.h>
 #include "repeat.h"
+#include "rdtsc.h"
 
 // #define MAX_SIZE (512*1024*1024)
-#define MAX_SIZE (512)
-#define vStride 128
+#define MAX_SIZE (8192)
+#define vStride 1024
 #define hStride 64
 
 pthread_mutex_t S;
@@ -24,29 +25,38 @@ void *Trojan(void *arrg){
   pthread_t thread;
   thread = pthread_self();
   CPU_ZERO(&my_set);       //clears the cpuset
-  CPU_SET(2, &my_set); //set CPU 2 on cpuset
+  CPU_SET(7, &my_set); //set CPU 2 on cpuset
   pthread_setaffinity_np(thread, sizeof(cpu_set_t), &my_set);
+
   clock_t start, end;
   double total_time;
   int i = 0;
   int k = 0;
-  int p = 0;
+  int p = 16;
   int count = 0;
   int * arr = (int *)arrg;
 
 
-  for(k = 0; k < 14; k++){
+  for(k = 0; k < 10; k++){
     pthread_mutex_lock(&T);
-    printf("Trojan\n");
-    printf("Running on core: %d\n", sched_getcpu());
-    start = clock();
-    for (i = 0; i < MAX_SIZE; i++){
-        repeat16(p = arr[p];)
+    printf("Trojan: ");
+    if (k%2 == 0){
+      // printf("Transmitting 0\n");
+      printf("0\n");
     }
-    end = clock();
+    else{
+      // printf("Running on core: %d\n", sched_getcpu());
+      start = rdtsc();
+      for (i = 0; i < MAX_SIZE; i++){
+          repeat16(p = arr[p];)
+      }
+      end = rdtsc();
+      total_time = ((double)(end-start))/CLOCKS_PER_SEC;
+      // printf("Trojan Start: %.1f ms.\n",(double)(start*1000));
+      // printf("Trojan End: %.1f ms.\n",(double)(end*1000));
+      printf("\n");
+    }
 
-    total_time = ((double)(end-start))/CLOCKS_PER_SEC;
-    printf("cpu time for loop 1 (k : %4d) %.1f ms.\n",k,(total_time*1000));
     pthread_mutex_unlock(&S);
   }
 }
@@ -56,7 +66,7 @@ void *Spy(void *arrg){
   pthread_t thread;
   thread = pthread_self();
   CPU_ZERO(&my_set);       //clears the cpuset
-  CPU_SET(2, &my_set);     //set CPU 2 on cpuset
+  CPU_SET(7, &my_set);     //set CPU 2 on cpuset
   pthread_setaffinity_np(thread, sizeof(cpu_set_t), &my_set);
   clock_t start, end;
   double total_time;
@@ -66,10 +76,10 @@ void *Spy(void *arrg){
   int count = 0;
   int * arr = (int *)arrg;
 
-  for(k = 0; k < 14; k++){
+  for(k = 0; k < 10; k++){
     pthread_mutex_lock(&S);
-    printf("Spy\n");
-    printf("Running on core: %d\n", sched_getcpu());
+    printf("Spy: ");
+    // printf("Running on core: %d\n", sched_getcpu());
     start = clock();
     count = 0;
     for (i = 0; i < MAX_SIZE; i++){
@@ -79,7 +89,10 @@ void *Spy(void *arrg){
     end = clock();
 
     total_time = ((double)(end-start))/CLOCKS_PER_SEC;
-    printf("cpu time for loop 1 (k : %4d) %.1f ms.\n",k,(total_time*1000));
+    // printf("Start: %.1f ms.\n",(double)(start*1000));
+    // printf("End: %.1f ms.\n",(double)(end*1000));
+    printf("Time: %.1f ms.\n",(double)(total_time*1000));
+
     pthread_mutex_unlock(&T);
   }
 }
@@ -87,47 +100,52 @@ void *Spy(void *arrg){
 void start(int *arr, int *arr2){
   pthread_t trojan;
   pthread_t spy;
-  int size = 1024;
+  int size = 1024*4;
+  int* clearCache = (unsigned int*)malloc(MAX_SIZE*4);
+  int temp = 0;
 
-  // sched_setaffinity(spy, sizeof(cpu_set_t), &my_set);
-    for (int i=0;i<512;i++)
-    {
-        arr[i] = i+vStride;  //placing values into the array, values are used to make sure that the values is always on the same cache line
-        if (arr[i] >= size) //check to see if the value exceeds the size of the array, so that we can move to the next cache line
-          arr[i] %= vStride;	 //moding by the stride allows to to maintain the current cache line
-    }
-    for (int i=0;i<512;i++)
-    {
-        arr2[i] = i+vStride; //Same as above
-        if (arr2[i] >= size)  //Same as above
-          arr2[i]= (arr2[i] % vStride); //Same as above
+  for(int x = 0; x < size; x++){
+    repeat64(temp = clearCache[temp];)
+  }
+  for (int i=0;i<size;i++)
+  {
+      arr[i] = i+vStride;  //placing values into the array, values are used to make sure that the values is always on the same cache line
+      if (arr[i] >= size) //check to see if the value exceeds the size of the array, so that we can move to the next cache line
+        arr[i] %= vStride;	 //moding by the stride allows to to maintain the current cache line
+  }
+  for (int i=0;i<size;i++)
+  {
+      arr2[i] = i+vStride; //Same as above
+      if (arr2[i] >= size)  //Same as above
+        arr2[i]= (arr2[i] % vStride); //Same as above
+  }
+
+
+
+  // for(int k = 0; k < 2; k++){
+    printf("Begin\n");
+    printf("Allocated Size: %li\n", MAX_SIZE*sizeof(int));
+    if(pthread_create(&trojan, NULL, Trojan, (void*)arr) || pthread_create(&spy, NULL, Spy, (void*)arr2)){
+      fprintf(stderr, "Error creating thread\n");
+      return;
     }
 
-    for(int k = 0; k < 2; k++){
-      printf("Begin\n");
-      printf("Allocated Size: %li\n", MAX_SIZE*sizeof(int));
-      if(pthread_create(&trojan, NULL, Trojan, (void*)arr) || pthread_create(&spy, NULL, Spy, (void*)arr2)){
-        fprintf(stderr, "Error creating thread\n");
-        return;
-      }
-
-      pthread_join(trojan, NULL);
-      pthread_join(spy, NULL);
-    }
+    pthread_join(trojan, NULL);
+    pthread_join(spy, NULL);
+  // }
 
 }
 
 int main(void)
 {
-  // clock_t start, end;
-  // sched_setaffinity(getpid(), sizeof(my_set), &my_set);
+
   double total_time;
   int i = 0;
   int k = 0;
   int count = 0;
 
-  int *arr = (int*)malloc(MAX_SIZE);
-  int *arr2 = (int*)malloc(MAX_SIZE);
+  int *arr = (unsigned int*)malloc(MAX_SIZE*4);
+  int *arr2 = (unsigned int*)malloc(MAX_SIZE*4);
 
   pthread_mutex_lock(&T);
   start(arr, arr2);
--- getCacheInfo.cpp
+++ getCacheInfo.cpp
@@ -1,33 +1,51 @@
-#include <sched.h>
-#include <stdio.h>
-#include <sys/time.h>
-#include <time.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/resource.h>
-#include <pthread.h>
-#include <errno.h>
-#include "repeat.h"
 #include <iostream>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-  clock_t start, end;
-  int i = 200;
-  int diff = 0;
-  int past = 0;
-  while(i < 256000){
-    start = clock();
-    int *arr = (int*)malloc(i);
-    end = clock();
-    diff = end-start;
-    if(diff > past + 5){
-      cout << "i: " << i << ", Time: ";
-      cout << end-start << endl;
-    }
-    past = diff;
-    i += 50;
-  }
-  return 0;
+int MASK_LSB_8 = 0xFF;
+
+void int2bin(int val) {
+	char buffer[33];
+	atol(val, buffer, 2);
+	printf("binary: %s\n", buffer);
+}
+
+// CPUID instruction takes no parameters as CPUID implicitly uses the EAX register.
+// The EAX register should be loaded with a value specifying what information to return
+void cpuinfo(int code, int *eax, int *ebx, int *ecx, int *edx) {
+	__asm__ volatile(
+			"cpuid;" //  call cpuid instruction
+			:"=a"(*eax),"=b"(*ebx),"=c"(*ecx), "=d"(*edx)// output equal to "movl  %%eax %1"
+			:"a"(code)// input equal to "movl %1, %%eax"
+			//:"%eax","%ebx","%ecx","%edx"// clobbered register
+	);
+}
+
+int main() {
+	int eax = 0, ebx = 0, ecx = 0, edx = 0;
+	//CPUID(0): Basic information
+	cpuinfo(0x0, &eax, &ebx, &ecx, &edx);
+
+	// check vendor [GenuineIntel, GenuntelineI] = ebx+ecx+edx
+	char vendor[13] = { 0 };
+	int identity_reg[3] = { ebx, ecx, edx };
+	for (unsigned int i = 0; i < 3; i++) {
+		for (unsigned int j = 0; j < 4; j++) {
+			vendor[i * 4 + j] = identity_reg[i] >> (j * 8) & MASK_LSB_8;
+		}
+	}
+	printf("\nVendor name = %s\n", vendor);
+
+	//CPUID(0x80000000): Extended CPU information
+	cpuinfo(0x80000000, &eax, &ebx, &ecx, &edx);
+	int intelExtended = eax & MASK_LSB_8;
+	printf("\nMax feature id = %d\n", intelExtended);
+	if (intelExtended < 6) {
+		printf("\nCache Extended Feature not supported");
+		return 0;
+	}
+	// CPUID(0x800000006): Cache line information
+	cpuinfo(0x80000006, &eax, &ebx, &ecx, &edx);
+	// We are only interested in lower 8 bits
+	printf("\nL2 Cache Size = %d\n", (ecx & MASK_LSB_8));
 }
